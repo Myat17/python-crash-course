580e409b9ccb5c0d6beddc38a5e922e0781549f0
Myat Hsu Thin <myathsuthin16@gmail.com>
2026-06-08 20:41:31 +0700

    Add two exercises from unit 9.

---
+++ exercise_8_1.py
@@ -0,0 +1,6 @@
+# Exercise 8-1
+# Write a function that prints one sentence telling everyone what you are learning about.
+def dispaly_message():
+    print("I am learning Python function.")
+
+dispaly_message()
---
+++ exercise_8_2.py
@@ -0,0 +1,7 @@
+# Exercise 8-2
+# Favourite book
+# Write a function that accepts one parameter, title
+def favourite_book(title):
+    print(f"One of my favorite books is {title.title()}.")
+
+favourite_book("atomic habit")
